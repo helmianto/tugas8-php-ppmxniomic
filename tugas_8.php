01ef7e89d794ae5d9b9705cdc6ce4a9276582cab
Helmianto <helmianto15@gmail.com>
2021-07-16 14:37:00 +0700

    add underscore

--- tugas_8.php
+++ tugas_8.php
@@ -1,18 +1,24 @@
 <?php
 function cetak_belah_ketupat1(){
 echo"<table><tr><td align='center'>";
+    print('__________');
+    echo"<br/>";
     for($i=0;$i<=9;$i++){
+        print('__________');
         for($a=$i;$a>=0;$a--){
             print('XX');
         }
-    echo"<br/>";
+        echo"<br/>";
     }
     for($i=0;$i<=8;$i++){
+        print('__________');
         for($a=$i;$a<=8;$a++){
             print('XX');
         }
-    echo"<br/>";
+        echo"<br/>";
     }
+    print('__________');
+    echo"<br/>";
     echo"</td></tr></table>";
 }
 
